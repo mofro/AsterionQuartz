d393f78aebbfbfdbf73ccca6fab355f6c68bbef6
Maurice Gaston <github@mofro.com>
2025-11-13 15:17:18 -0500

    removed the broken baseUrl setting.

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: process.env.BASE_URL || "/",
     ignorePatterns: ["private", "templates", ".obsidian", "_notes", "**/_templates"],
     defaultDateType: "modified",
     theme: {
